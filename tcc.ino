214bed5c0e26c0d358a9ed9a3c13787231c7bb3e
AlanJs26 <alanjoses.29@gmail.com>
2021-10-12 11:09:15 -0300

    before switching from wifimanager to Wifi library

--- tcc.ino
+++ tcc.ino
@@ -4,18 +4,21 @@
 #include <WebServer.h>
 #include <WiFiManager.h>
 #include <addons/RTDBHelper.h>
+#include <addons/TokenHelper.h>
 /*#include <cstdio>*/
 
 WiFiManager wifiManager;
 
 #define API_KEY "AIzaSyAcT5oGP6ZFtJ5sHXsqxVa6DwgwxBzVexw"
 #define DATABASE_URL "remotecamera-6f583-default-rtdb"
+#define DATABASE_SECRET "zC7bJfF0iGMQ1IPKq5bYj2WOrAx9UF9j9ghzaoTq"
 
 FirebaseData fbdo;
 FirebaseData stream;
 FirebaseAuth auth;
 
 FirebaseConfig config;
+FirebaseJson json;
 
 bool signupOK = false;
 
@@ -53,25 +56,31 @@ void setup() {
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  config.signer.tokens.legacy_token = DATABASE_SECRET;
+
+  /*fbdo.setResponseSize(4096);*/
+  /*config.token_status_callback = tokenStatusCallback;*/
+  /*config.max_token_generation_retry = 5;*/
+
+  Firebase.reconnectWiFi(true);
+
   Serial.println("Abertura Portal");
-  if (wifiManager.autoConnect() ||
-      wifiManager.startConfigPortal("ESP32-CONFIG", "12345678")) {
+  if (wifiManager.autoConnect("ESP32-CONFIG", "12345678")
+      /*|| wifiManager.startConfigPortal("ESP32-CONFIG", "12345678")*/
+  ) {
     Serial.println("Conectado na Rede!!!");
 
     /*Serial.println("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);*/
 
-    config.api_key = API_KEY;
-    config.database_url = DATABASE_URL;
-
-    Serial.print("Signing up new Anonymous user...");
+    Serial.print("Signing up new Anonymous user... ");
     if (Firebase.signUp(&config, &auth, "", "")) {
       Serial.println("Signed!!");
       signupOK = true;
     } else {
       Serial.printf("%s\n", config.signer.signupError.message.c_str());
     }
-
-    Firebase.begin(&config, &auth);
 
     /*String uid = auth.token.uid.c_str();*/
     /*String connectedWithPath = "/users/" + uid + "/connectedWith";*/
@@ -83,9 +92,10 @@ void setup() {
 
   } else {
     Serial.println("Falha ao conectar. Reiniciando...");
-    delay(20000);
+    /*delay(20000);*/
     ESP.restart();
   }
+  Firebase.begin(&config, &auth);
   Serial.println("exited");
 }
 
@@ -110,17 +120,16 @@ void loop() {
     // connected to wifi and firebase
   } else if (WiFi.status() == WL_CONNECTED && signupOK && Firebase.ready()) {
     digitalWrite(26, HIGH);
-    Serial.println("before timeout");
 
     /*delay(10000);*/
-    if (stream.streamTimeout()) {
-      Serial.println("stream timed out, resuming...\n");
-      /*delay(10000);*/
+    /*if (stream.streamTimeout()) {*/
+    /*Serial.println("stream timed out, resuming...\n");*/
+    /*delay(10000);*/
 
-      if (!stream.httpConnected())
-        Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(),
-                      stream.errorReason().c_str());
-    }
+    /*if (!stream.httpConnected())*/
+    /*Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(),*/
+    /*stream.errorReason().c_str());*/
+    /*}*/
 
     if (state == 1) {
       Serial.println("Adding random code");
@@ -129,33 +138,45 @@ void loop() {
 
       char code[5] = "";
       gen_random(code, 4);
-      char codePath[] = "/cameras/codes/";
+      char codePath[] = "/cameras/codes";
       char fullCodePath[20];
-      sprintf(fullCodePath, "%s%s", codePath, code);
+      sprintf(fullCodePath, "%s/%s", codePath, code);
 
-      Serial.println(fullCodePath);
+      /*Serial.println(fullCodePath);*/
+      Serial.printf("\"%s\": \"%s\"\n", fullCodePath, uid.c_str());
       /*delay(10000);*/
 
-      Firebase.setString(fbdo, fullCodePath, uid.c_str());
+      /*json.set(code, uid.c_str());*/
+      json.set("alan", "joselegal");
+
+      /*Firebase.setString(fbdo, "/cameras/codes/bq3w", uid.c_str());*/
+      /*Firebase.setString(fbdo, "/cameras/codes/bq3w", "teste");*/
+      if (Firebase.pushJSON(fbdo, "/cameras/codes", json)) {
+        Serial.println("nice... i think.");
+      } else {
+        Serial.println(fbdo.errorReason());
+      }
       state = 2;
     } else if (state == 2) {
-      if (stream.streamAvailable()) {
-        Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent "
-                      "type, %s\n\n",
-                      stream.streamPath().c_str(), stream.dataPath().c_str(),
-                      stream.dataType().c_str(), stream.eventType().c_str());
-        printResult(stream); // see addons/RTDBHelper.h
-        Serial.println();
+      /*if (stream.streamAvailable()) {*/
+      /*Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent "*/
+      /*"type, %s\n\n",*/
+      /*stream.streamPath().c_str(), stream.dataPath().c_str(),*/
+      /*stream.dataType().c_str(), stream.eventType().c_str());*/
+      /*printResult(stream); // see addons/RTDBHelper.h*/
+      /*Serial.println();*/
 
-        // This is the size of stream payload received (current and max value)
-        // Max payload size is the payload size under the stream path since the
-        // stream connected and read once and will not update until stream
-        // reconnection takes place. This max value will be zero as no payload
-        // received in case of ESP8266 which BearSSL reserved Rx buffer size is
-        // less than the actual stream payload.
-        Serial.printf("Received stream payload size: %d (Max. %d)\n\n",
-                      stream.payloadLength(), stream.maxPayloadLength());
-      }
+      /*// This is the size of stream payload received (current and max value)*/
+      /*// Max payload size is the payload size under the stream path since
+       * the*/
+      /*// stream connected and read once and will not update until stream*/
+      /*// reconnection takes place. This max value will be zero as no payload*/
+      /*// received in case of ESP8266 which BearSSL reserved Rx buffer size
+       * is*/
+      /*// less than the actual stream payload.*/
+      /*Serial.printf("Received stream payload size: %d (Max. %d)\n\n",*/
+      /*stream.payloadLength(), stream.maxPayloadLength());*/
+      /*}*/
     }
 
     // not connected at all
